7edd9ce7a6603eaf2812b6fe577aafd45235b37d
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-11-04 14:20:53 +0000

    Changes to be made

--- gene_pred_Scripts/busco_s11.sh
+++ gene_pred_Scripts/busco_s11.sh
@@ -5,11 +5,11 @@
 #Run in conda env - BUSCOenv
 #Ran on genome(softmasked) and gene models (final_genes_appended_renamed.gene.fasta)
 
-for Assembly in $(ls gene_pred/codingquary/F.oxysporum_fsp_fragariae/DSA14_003/final/final_genes_appended_renamed.gene.fasta); do
+for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/Pilon_SDen/FolR1_SDen_pilon.fasta); do
 Strain=$(echo $Assembly| rev | cut -d '/' -f3 | rev)
 Organism=$(echo $Assembly | rev | cut -d '/' -f4 | rev)
 echo "$Organism - $Strain"
-ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Gene_prediction
+ProgDir=~/git_repos/fusarium_ex_strawberry/ProgScripts
 BuscoDB=$(ls -d /projects/dbBusco/sordariomycetes_odb10)
 OutDir=$(dirname $Assembly)/busco_sordariomycetes_obd10
 sbatch $ProgDir/busco.sh $Assembly $BuscoDB $OutDir
