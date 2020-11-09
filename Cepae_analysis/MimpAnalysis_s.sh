49be24097d847fbcb2a13a49dfecc3b20eaf472b
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-11-09 16:49:13 +0000

    Made a horrific oversight in the input path - ran again and is sorted now

--- Cepae_analysis/MimpAnalysis_s.sh
+++ Cepae_analysis/MimpAnalysis_s.sh
@@ -2,12 +2,14 @@
 # Run in Repenv
 # used full paths for input files
 # Use diff out directory - non writable
+# You messed up on last script run you used repeat_masked/F.oxysporum_fsp_cepae/Fus2_canu_new/edited_contigs_repmask/Fus2_canu_contigs_softmasked_repeatmasker_TPSI_appended.fa
+# Should have used repeat_masked/F.oxysporum_fsp_cepae/Fus2_canu_new/edited_contigs_repmask/Fus2_canu_contigs_unmasked.fa
 
-for Assembly in $(ls repeat_masked/F.oxysporum_fsp_cepae/Fus2_canu_new/edited_contigs_repmask/Fus2_canu_contigs_softmasked_repeatmasker_TPSI_appended.fa); do
+for Assembly in $(ls repeat_masked/F.oxysporum_fsp_cepae/Fus2_canu_new/edited_contigs_repmask/Fus2_canu_contigs_unmasked.fa); do
     Organism=$(echo "$Assembly" | rev | cut -d '/' -f4 | rev)
     Strain=$(echo "$Assembly" | rev | cut -d '/' -f3 | rev)
     GeneGff=$(ls gene_pred/final_genes/F.oxysporum_fsp_cepae/Fus2_canu_new/final/final_genes_appended_renamed.gff3)
-    OutDir=../../../../../fusarium_ex_strawberry/F.oxysporum_fsp_cepae/Fus2_canu_new/mimps/V2
+    OutDir=../../../../../fusarium_ex_strawberry/F.oxysporum_fsp_cepae/Fus2_canu_new/mimps/V3
     mkdir -p "$OutDir"
     echo "$Organism - $Strain"
     ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Feature_annotation
