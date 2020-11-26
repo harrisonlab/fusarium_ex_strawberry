b665696d6c6ea9938332eae43f2b66a11c295918
AkinyanjuA20 <akinkunmiakinyanju@niab.com>
2020-11-26 16:42:56 +0000

    Updates

--- olc_assemblers/Pilon-R2.sh
+++ olc_assemblers/Pilon-R2.sh
@@ -3,19 +3,21 @@
 # Make sure script is executable
 # chmod u+x ./sub_pilon.sh
 #ran in a different folder to test pilon, because the path are different
+# assembly/SMARTdenovo/F.oxysporum_fsp_fragariae/DSA14_003/medaka/DSA14_003_smartdenovo_racon_round_4_renamed.fasta
+# assembly/flye/F.oxysporum_fsp_fragariae/DSA14_003/medaka/assembly_racon_round_3_renamed.fasta
 
 ```bash
-for Assembly in $(ls race_1_smartdenovo_racon_round_10_renamed.fasta); do
-  Organism=F.oxysporum_fsp_lactucae
-  Strain=AJ520
-  IlluminaDir=$(ls -d $Strain)
+for Assembly in $(ls assembly/flye/F.oxysporum_fsp_fragariae/DSA14_003/medaka/assembly_racon_round_3_renamed.fasta); do
+  Organism=F.oxysporum_fsp_fragariae
+  Strain=DSA14_003
+  IlluminaDir=$(ls -d ../raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003)
   echo $Strain
   echo $Organism
-  TrimF1_Read=$(ls $IlluminaDir/F/*_trim.fq.gz | head -n2 | tail -n1);
-  TrimR1_Read=$(ls $IlluminaDir/R/*_trim.fq.gz | head -n2 | tail -n1);
+  TrimF1_Read=$(ls $IlluminaDir/F/14-003_S4_L001_R1_001.fastq.gz | head -n2 | tail -n1);
+  TrimR1_Read=$(ls $IlluminaDir/R/14-003_S4_L001_R2_001.fastq.gz| head -n2 | tail -n1);
   echo $TrimF1_Read
   echo $TrimR1_Read
-  OutDir=$(dirname $Assembly)
+  OutDir=assembly/SMARTdenovo/F.oxysporum_fsp_fragariae/DSA14_003/pilon
   Iterations=10
   ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Genome_assemblers/pilon
   sbatch $ProgDir/sub_pilon.sh $Assembly $TrimF1_Read $TrimR1_Read $OutDir $Iterations
@@ -40,6 +42,6 @@ done
 # sbatch $ProgDir/sub_pilon.sh $Assembly $FileF $FileR $OutDir $Iterations
 # done
 
-# For fragariae
+# For fragariae - ../raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003/F/14-003_S4_L001_R1_001.fastq.gz
 #/projects/fusarium_ex_strawberry/raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003/F/14-003_S4_L001_R1_001.fastq.gz
 # /projects/fusarium_ex_strawberry/raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003/R/14-003_S4_L001_R2_001.fastq.gz
