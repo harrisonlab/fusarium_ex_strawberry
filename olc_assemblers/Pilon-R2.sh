40da3e98d4f09df8a4e32c02ecc8bbbb619e8356
AkinyanjuA20 <akinkunmiakinyanju@niab.com>
2020-11-26 16:30:25 +0000

    changes

--- olc_assemblers/Pilon-R2.sh
+++ olc_assemblers/Pilon-R2.sh
@@ -6,22 +6,30 @@
 
 ```bash
 for Assembly in $(ls race_1_smartdenovo_racon_round_10_renamed.fasta); do
-Organism=F.oxysporum_fsp_lactucae
-Strain=AJ520
-IlluminaDir=$(ls -d $Strain)
-echo $Strain
-echo $Organism
-TrimF1_Read=$(ls $IlluminaDir/F/*_trim.fq.gz | head -n2 | tail -n1);
-TrimR1_Read=$(ls $IlluminaDir/R/*_trim.fq.gz | head -n2 | tail -n1);
-echo $TrimF1_Read
-echo $TrimR1_Read
-OutDir=$(dirname $Assembly)
-Iterations=10
-ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Genome_assemblers/pilon
-sbatch $ProgDir/sub_pilon.sh $Assembly $TrimF1_Read $TrimR1_Read $OutDir $Iterations
+  Organism=F.oxysporum_fsp_lactucae
+  Strain=AJ520
+  IlluminaDir=$(ls -d $Strain)
+  echo $Strain
+  echo $Organism
+  TrimF1_Read=$(ls $IlluminaDir/F/*_trim.fq.gz | head -n2 | tail -n1);
+  TrimR1_Read=$(ls $IlluminaDir/R/*_trim.fq.gz | head -n2 | tail -n1);
+  echo $TrimF1_Read
+  echo $TrimR1_Read
+  OutDir=$(dirname $Assembly)
+  Iterations=10
+  ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Genome_assemblers/pilon
+  sbatch $ProgDir/sub_pilon.sh $Assembly $TrimF1_Read $TrimR1_Read $OutDir $Iterations
 done
+
   # You might rename your contigs at this point using remove_contaminants.py
-```
+
+  ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Assembly_qc
+      touch tmp.txt
+      for Assembly in $(ls assembly/...); do
+          OutDir=$(dirname $Assembly)
+          $ProgDir/remove_contaminants.py --inp $Assembly --out $OutDir/race_1_smartdenovo_racon_round_3_renamed.fasta --coord_file tmp.txt > $OutDir/log.txt
+      done
+      rm tmp.txt
 
 #for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/Pilon_SDen/race_1_smartdenovo_racon_round_10_renamed.fasta); do
 #  FileF=$(ls ../oldhome/groups/harrisonlab/project_files/fusarium/raw_dna/paired/F.oxysporum_fsp_lactucae/AJ520/F/AJ520_S2_L001_R1_001.fastq.gz)
@@ -31,3 +39,7 @@ done
 # ProgDir=/home/akinya/git_repos/fusarium_ex_strawberry/ProgScripts
 # sbatch $ProgDir/sub_pilon.sh $Assembly $FileF $FileR $OutDir $Iterations
 # done
+
+# For fragariae
+#/projects/fusarium_ex_strawberry/raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003/F/14-003_S4_L001_R1_001.fastq.gz
+# /projects/fusarium_ex_strawberry/raw_dna/paired/F.oxysporum_fsp_fragariae/DSA14_003/R/14-003_S4_L001_R2_001.fastq.gz
