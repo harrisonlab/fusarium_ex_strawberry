0917534c5df5e327571acda49c8dd5e0da2d35a7
AkinyanjuA20 <akinkunmiakinyanju@niab.com>
2020-12-18 12:09:00 +0000

    Identifying TE families on potential PS/LS contig

--- RepeatMask/family_identifier.sh
+++ RepeatMask/family_identifier.sh
@@ -24,3 +24,21 @@ Sort no. TEs by family and function
 #grep -E ‘extra|value|service’ sample.txt
 #egrep ‘extra|value|service’ sample.txt
 #grep -e extra -e value -e service sample.txt
+
+# Ran repeatmasker on contig 12 fasta
+# repeat_masked/F.oxysporum_fsp_fragariae/DSA14_003/flye/ncbi_edits_repmask/contig_12/DSA14_003_RepMod-families.stk
+  ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Repeat_masking
+  BestAssembly=assembly/flye/F.oxysporum_fsp_fragariae/DSA14_003/pilon/contig_12.fasta
+  OutDir=repeat_masked/F.oxysporum_fsp_fragariae/DSA14_003/flye/ncbi_edits_repmask/contig_12
+  sbatch $ProgDir/rep_modeling.sh $BestAssembly $OutDir
+  sbatch $ProgDir/transposonPSI.sh $BestAssembly $OutDir
+
+# pulling families just on contig 12 repeat_masked/F.oxysporum_fsp_fragariae/DSA14_003/flye/ncbi_edits_repmask/contig_12/DSA14_003_RepMod-families.stk
+# search for diff values: Retrotransposon , Retrotransposed_Element , DNA_Transposon
+
+for TEfamily in $(ls repeat_masked/F.oxysporum_fsp_fragariae/DSA14_003/flye/ncbi_edits_repmask/contig_12/DSA14_003_RepMod-families.stk); do
+  Organism=$(echo $Antismash | rev | cut -f5 -d '/' | rev)
+  Strain=$(echo $Antismash | rev | cut -f4 -d '/' | rev)
+  echo "$Organism - $Strain"
+  cat $TEfamily | grep 'Retrotransposed_Element' | sort | uniq -c
+done
